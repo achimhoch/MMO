585b9d28bf9e06ca97a1af39b6eadd735e615adf
achimhoch <achim-hoch@web.de>
2026-06-23 07:59:57 +0200

    update

--- server/manager/TiledChunkManager.js
+++ server/manager/TiledChunkManager.js
@@ -5,7 +5,15 @@ class TiledChunkManager {
 
     constructor() {
 
-        this.map = JSON.parse(fs.readFileSync("../../client/assets/maps/continents/grassland.json"));
+        this.chunkSize = 16;
+        this.map = JSON.parse(fs.readFileSync("../../client/assets/maps/continents/grassland2.json"));
+        this.width = this.map.width;
+        this.height = this.map.height;
+        this.layers = {};
+
+        for (const layer of this.layers) {
+            this.layers[layer.name] = layer.data
+        }
     }
 
     loadChunk(chunkX, chunkY) {
@@ -14,11 +22,37 @@ class TiledChunkManager {
             x: chunkX,
             y: chunkY,
             layers: {
-                ground: [],
-                objects: [],
-                collision: []
+                ground: this.extractLayer("Ground", chunkX, chunkY),
+                objects: this.extractLayer("Objects", chunkX, chunkY),
+                collision: this.extractLayer("Collision", chunkX, chunkY)
             }
         };
+    }
+
+    extractLayer(layerName, chunkX, chunkY) {
+        const layer = this.layers[layerName];
+        if (!layer) {
+            return [];
+        }
+        const tiles = [];
+        const startX = chunkX * this.chunkSize;
+        const startY = chunkY * this.chunkSize;
+
+        for (let y = 0; y < this.chunkSize; y++) {
+            for (x = 0; x < this.chunkSize; x++) {
+                const worldX = startX + x;
+                const worldY = startY + y;
+
+                if (worldX < 0 || worldY < 0 || worldX >= this.width || worldY >= this.height) {
+                    tiles.push(0);
+                    continue;
+                }
+                const index = worldY * this.width + worldX;
+                tiles.push(layer[index]);
+            }
+        }
+
+        return tiles;
     }
 }
 
