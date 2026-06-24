6e84e8bbb5734b1ff44a055e6d9b7dcb30928ad8
achimhoch <achim-hoch@web.de>
2026-06-24 16:09:32 +0200

    update

--- client/js/rendering/ChunkRenderer.js
+++ client/js/rendering/ChunkRenderer.js
@@ -19,13 +19,24 @@ export default class ChunkRenderer {
                 const worldY = chunk.y * size + y;
                 const pos = IsoMath.worldToIso(worldX , worldY); 
                 //console.log(pos);
-                const sprite = this.scene.add.sprite(pos.x, pos.y, 'tiles', tile -1);
+                const ground = this.scene.add.sprite(pos.x, pos.y, 'tiles', tile -1);
                 //sprite.depth = IsoMath.depth(pos.x, pos.y);
-                sprite.depth = worldY *100 + worldX;
-                container.add(sprite);
+                ground.depth = worldY * 200 + worldX;
+                container.add(ground);
                 //this.scene.add.existing(sprite);
             }
         });
+
+        chunk.layers.objects.forEach((obTile, obIndex) => {
+            const x = obIndex % size;
+            const y = Math.floor(obIndex / size);
+            const obTileX = chunk.x * size + x;
+            const obTileY = chunk.y * size + y;
+            const obPos = IsoMath.worldToIso(obTileX, obTileY);
+            const objects = this.scene.add.sprite(obPos.x, obPos.y);
+            objects.depth = obPos.y + 150 + obPos.x;
+            container.add(objects);
+        })
         //console.log(container);
         return container;
     }   
